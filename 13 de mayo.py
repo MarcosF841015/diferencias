e28090f2f4bd3a063864e606f7f3f9af8fd2a540
Marcos2 <MarcosS151084>
2024-05-15 19:31:23 -0300

    diferencias

---
+++ 13 de mayo.py
@@ -0,0 +1,10 @@
+cad1 = "hola"
+cad2 = "mundo"
+if cad1 == cad2:
+    print("son iguales")
+else:
+    print("no son iguales")
+if cad1 != "hello":
+    print("no es la palabra hello...")
+else:
+    pint("es la palabra hello...")
